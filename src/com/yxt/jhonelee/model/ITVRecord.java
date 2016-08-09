2eb8f45661d7195c3c9cb0ce642a955d03718eb1
JhoneLeedx <lijing930615@live.com>
2016-08-09 14:52:34 +0800

    ..

---
+++ src/com/yxt/jhonelee/model/ITVRecord.java
@@ -0,0 +1,48 @@
+package com.yxt.jhonelee.model;
+
+/**
+ * 
+ * @author JhoneLee
+ * @category 管理员类
+ *
+ */
+public class ITVRecord {
+
+	private int mAdminId;
+	private int mId;
+	private int mRegistId;
+	private String mReason;
+
+	public int getmAdminId() {
+		return mAdminId;
+	}
+
+	public void setmAdminId(int mAdminId) {
+		this.mAdminId = mAdminId;
+	}
+
+	public int getmId() {
+		return mId;
+	}
+
+	public void setmId(int mId) {
+		this.mId = mId;
+	}
+
+	public int getmRegistId() {
+		return mRegistId;
+	}
+
+	public void setmRegistId(int mRegistId) {
+		this.mRegistId = mRegistId;
+	}
+
+	public String getmReason() {
+		return mReason;
+	}
+
+	public void setmReason(String mReason) {
+		this.mReason = mReason;
+	}
+
+}
