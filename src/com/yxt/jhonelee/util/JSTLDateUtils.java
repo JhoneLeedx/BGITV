e62881e40a104616806e5b57cbc9c28dafe548de
JhoneLeedx <lijing930615@live.com>
2016-08-11 22:09:07 +0800

    时间改正成功

--- src/com/yxt/jhonelee/util/JSTLDateUtils.java
+++ src/com/yxt/jhonelee/util/JSTLDateUtils.java
@@ -3,69 +3,67 @@ package com.yxt.jhonelee.util;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
 
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.TagSupport;
 
 /**
  * 用于页面jstl时间格式化 //自定义jstl时间格式化标签
+ * 
  * @author JhoneLee
  *
  */
 public class JSTLDateUtils extends TagSupport {
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = -3354015192721342312L;
-    
-    
-    private String value;
-    private String parttern;
-    public void setValue(String value) {
-        this.value = value;
-    }
-    public void setParttern(String parttern ) {
-        this.parttern =  parttern ;
-    }
-    public int doStartTag() throws JspException{
-        String vv = String.valueOf(value);  
-        long time = Long.valueOf(vv);  
-        Calendar c = Calendar.getInstance();  
-        c.setTimeInMillis(time);  
-        SimpleDateFormat dateformat =new SimpleDateFormat(parttern);  
-        String s = dateformat.format(c.getTime());  
-        try {  
-            pageContext.getOut().write(s);  
-        } catch (IOException e) {  
-            e.printStackTrace();  
-        }  
-        return super.doStartTag();
-    }
-    public static void main(String[] args) {
-    	
-    	Date now = new Date();
-    	SimpleDateFormat formatter = new SimpleDateFormat( "yyyy-MM-dd HH:mm:ss");
-    	
-    	Date userdate;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3354015192721342312L;
+
+	private String value;
+	private String parttern;
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public void setParttern(String parttern) {
+		this.parttern = parttern;
+	}
+
+	public int doStartTag() throws JspException {
+		String vv = String.valueOf(value);
+		long time = Long.valueOf(vv);
+		long days = time / (1000 * 60 * 60 * 24);
+		long hours = (time-days*(1000 * 60 * 60 * 24))/(1000* 60 * 60);
+		long minutes = (time-days*(1000 * 60 * 60 * 24)-hours*(1000* 60 * 60))/(1000* 60);
+		SimpleDateFormat dateformat = new SimpleDateFormat(parttern);
+		String s = days+"天"+hours+"小时"+minutes+"分";
 		try {
+			pageContext.getOut().write(s);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return super.doStartTag();
+	}
+
+	public static void main(String[] args) {
+
+		Date now = new Date();
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		try {
+			Date userdate;
 			userdate = formatter.parse("2016-08-11 15:48:30");
-		
-    	long a = now.getTime()-userdate.getTime();
-    	
-         long time = Long.valueOf(a);  
-            Calendar c = Calendar.getInstance();
-            c.setTimeZone(TimeZone.getTimeZone("GMT+0"));
-            c.setTimeInMillis(time);  
-            SimpleDateFormat dateformat =new SimpleDateFormat("dd HH:mm");  
-            String s = dateformat.format(c.getTime());  
-        System.out.println(s);
+			long diff = now.getTime() - userdate.getTime();
+			long days = diff / (1000 * 60 * 60 * 24);
+			long hours = (diff-days*(1000 * 60 * 60 * 24))/(1000* 60 * 60);
+			long minutes = (diff-days*(1000 * 60 * 60 * 24)-hours*(1000* 60 * 60))/(1000* 60);
+			System.out.println(days+"天"+hours+"小时"+minutes+"分");
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    }
+
+	}
 }
