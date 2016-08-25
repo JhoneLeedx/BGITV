c1f8b20b1dfeca807a8089c002cd1db7f365ba6e
JhoneLeedx <lijing930615@live.com>
2016-08-25 20:58:32 +0800

    ss

--- src/com/yxt/jhonelee/mapper/DoctorMapper.java
+++ src/com/yxt/jhonelee/mapper/DoctorMapper.java
@@ -2,11 +2,13 @@ package com.yxt.jhonelee.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
+
 import com.yxt.jhonelee.model.DocDetail;
 
 
 public interface DoctorMapper {
 
 	public List<DocDetail> findAllDoc(int hospitalId);//找到某个医院下所有医生信息
-	public List<DocDetail> findAllDoctor(List<Integer> list);//找到管理员下所有医生信息
+	public List<DocDetail> findAllDoctor(@Param(value = "list")List<Integer> list);//找到管理员下所有医生信息
 }
